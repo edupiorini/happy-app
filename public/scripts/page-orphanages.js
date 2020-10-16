970818b2f03cb19ff3b19d2f16223c84c4dd01fc
edupiorini <eduardopiorini@hotmail.com>
2020-10-16 20:46:45 -0300

    popup refactored

--- public/scripts/page-orphanages.js
+++ public/scripts/page-orphanages.js
@@ -1,16 +1,25 @@
-const map = L.map("mapid").setView([-23.0863016,-45.5070517], 15);
-
+const map = L.map('mapid').setView([-23.02618, -45.5559945], 12);
 
 //create and add tileLayer
-L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {}).addTo(
-  map
-);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
 
 //create icon
+const icon = L.icon({
+  iconUrl: './public/images/map-marker.svg',
+  iconSize: [58, 68],
+  iconAnchor: [29, 68],
+  popupAnchor: [170, 2]
+})
+//create popup overlay
+
+const popup = L.popup({
+  closeButton: false,
+  className: 'map-popup',
+  minWidth: 240,
+  minHeight: 240
+}).setContent('Lar das Meninas <a href="orphanage.html?id=1" class = "choose-orphanage"> <img src="./public/images/arrow-white.svg"></a>');
 
 
+//create and add marker
 
-L.marker([-23.0863016,-45.5070517])
-  .addTo(map)
-  .bindPopup("A pretty CSS3 popup.<br> Easily customizable.")
-  .openPopup();
+L.marker([-23.02618, -45.5559945],{icon: icon}).addTo(map).bindPopup(popup);
