45aa8e575d6c461209b7089f3386613f90e8a4c7
edupiorini <eduardopiorini@hotmail.com>
2020-10-19 06:30:43 -0300

    template engine config

--- src/server.js
+++ src/server.js
@@ -1,13 +1,22 @@
 //import dependencies
-const express = require('express');
-const path = require('path')
-//init expŕess 
+const express = require("express");
+const path = require("path");
+
+//init expŕess
 const server = express();
+server
 
-//create path
-server.get('/', (request, response) => {
-    return response.sendFile(path.join(__dirname, 'views', 'index.html'));
-})
+    //using static files
+    .use(express.static("public"))
 
-//start server
-server.listen(5500)
+    //config temple engine
+    .set('views', path.join(__dirname,"views"))
+    .set('view engine', 'hbs')
+
+    //create path
+    .get("/", (request, response) => {
+        return response.render('index');
+    })
+
+    //start server
+    .listen(5500);
