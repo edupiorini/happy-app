df0a33bfb66801dc64bdd6c3414c5a3e4d8e7046
edupiorini <eduardopiorini@hotmail.com>
2020-10-20 07:01:36 -0300

    sync front with backend

--- src/server.js
+++ src/server.js
@@ -1,22 +1,24 @@
 //import dependencies
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
+const pages = require("./pages.js");
 
 //init expŕess
 const server = express();
 server
 
-    //using static files
-    .use(express.static("public"))
+  //using static files
+  .use(express.static("public"))
 
-    //config temple engine
-    .set('views', path.join(__dirname,"views"))
-    .set('view engine', 'hbs')
+  //config temple engine
+  .set("views", path.join(__dirname, "views"))
+  .set("view engine", "hbs")
 
-    //create path
-    .get('/', (request, response) => {
-        return response.render('index')
-    });
+  //create path
+  .get("/", pages.index)
+  .get("/orphanage", pages.orphanage)
+  .get("/create-orphanage", pages.createOrphanage)
+  .get("/orphanages", pages.orphanages);
 
-    //start server
-    server.listen(5500);
+//start server
+server.listen(5500);
